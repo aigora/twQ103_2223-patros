abd88159cd3cd62cd85221fa5ab85b9aa9799eef
Mario Calderón <101515518+mariocalderonupm@users.noreply.github.com>
2023-05-21 21:08:52 +0200

    Update TRABAJO.c

--- TRABAJO.c
+++ TRABAJO.c
@@ -270,10 +270,12 @@ int operaciones() {
 	char nombre[20];
 	int n;
 	
-	printf("Introduce el nombre del fichero para el distrito desado: (XXXXYY_barrio.txt ; XXXX = year, YY = month)\n");
+	printf("Introduce el nombre del fichero para el distrito deseado: (XXXXYY_barrio.txt ; XXXX = year, YY = month): ");
+	printf("\n");
 	scanf("%s", nombre);
 	
-	printf("Introduce el dato el cual desea calcular: (ph, conductividad, turbidez o coliforme)\n");
+	printf("Introduce el dato el cual desea calcular: (ph, conductividad, turbidez o coliforme): ");
+	printf("\n");
 	scanf("%s", categoria);
 	
 	printf("Seleccione una opcion:\n1: Calcular la media\n2: Calcular el maximo\n3:Calcular el minimo\n");
